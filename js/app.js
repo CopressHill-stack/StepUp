722854d64fea6c545340bf468aabb024a5ebcda8
CopressHill-stack <112204347+CopressHill-stack@users.noreply.github.com>
2025-01-27 19:19:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -284,7 +284,7 @@ function sendNotification(taskName){
     if(Notification.permission === 'granted'){
         new Notification('Нагадую тобі', {
             body: 'Не забудь виконати своє завдання: ' + taskName,
-            icon: "../img/icon32.png"
+            icon: "https://copresshill-stack.github.io/StepUp/img/icon32.png",
         });
     }else if(Notification.permission !== 'denied'){
         Notification.requestPermission().then(permission => {
